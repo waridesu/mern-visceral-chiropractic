898ef5cdc95f8090f57e705e28f53b159cd20801
Denis <waridesu@gmail.com>
2021-01-10 09:58:41 +0200

    clientStart

--- app.ts
+++ app.ts
@@ -3,12 +3,16 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 const app = express()
-
+app.use('/api/auth',require('./routes/auth-routes'))
 const PORT = config.get('port') || 5000
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUrl'), {})
+        await mongoose.connect(config.get('mongoUrl'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
     } catch (e) {
         console.log('Server Error:', e.message)
         process.exit()
---
+++ client/src/pages/CreatePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const CreatePage=()=>{
+    return <>
+        <h1>Create Page</h1>
+    </>
+}
